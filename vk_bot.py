37d918b27fd532591ac416e0701903945542a441
ilyashirko <ilyashirko@gmail.com>
2022-11-06 21:18:44 +0300

    setup echo mode

--- vk_bot.py
+++ vk_bot.py
@@ -1,26 +1,25 @@
-import vk_api
+import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 from environs import Env
+import random
 
 
-def message_handler(longpoll):
-    for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-            else:
-                print('От меня для: ', event.user_id)
-            print('Текст:', event.text)
-
-
-def run_vk_bot(vk_token):
-    vk_session = vk_api.VkApi(token=vk_token)
-    return VkLongPoll(vk_session)
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1,1000)
+    )
 
 
 if __name__ == '__main__':
     env = Env()
     env.read_env()
-    longpoll = run_vk_bot(env.str('VK_TOKEN'))
-    message_handler(longpoll)
+
+    vk_session = vk.VkApi(token=env.str('VK_TOKEN'))
+    vk_api = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+    
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
