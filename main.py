cde8359cc083b2ec3293f146d255deec50ff9624
ilyashirko <ilyashirko@gmail.com>
2022-11-06 15:24:32 +0300

    setup dialogflow

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from telegram.ext import Updater, Filters, CommandHandler, MessageHandler
 from environs import Env
+from google.cloud import dialogflow
+
+
+def detect_intent_text(project_id, session_id, text, language_code='ru-RU'):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    return response.query_result
 
 
 def start(update, context):
@@ -10,9 +22,14 @@ def start(update, context):
 
 
 def message_handler(update, context):
+    dialogflow_response = detect_intent_text(
+        env.str('GOOGLE_PROJECT_ID'),
+        update.effective_chat.id,
+        update.message.text,
+    )
     context.bot.send_message(
         update.effective_chat.id,
-        text=update.message.text
+        text=dialogflow_response.fulfillment_text
     )
 
 
