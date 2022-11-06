8041f1b78851fa278f45898b8d916faf5a97c833
ilyashirko <ilyashirko@gmail.com>
2022-11-06 21:36:24 +0300

    new func = run bot

--- vk_bot.py
+++ vk_bot.py
@@ -1,7 +1,9 @@
-import vk_api as vk
-from vk_api.longpoll import VkLongPoll, VkEventType
-from environs import Env
 import random
+
+import vk_api as vk
+from environs import Env
+from vk_api.longpoll import VkEventType, VkLongPoll
+
 from dialogflow_processing import detect_intent_text
 
 
@@ -20,14 +22,17 @@ def message_handler(event, vk_api):
     )
 
 
-if __name__ == '__main__':
-    env = Env()
-    env.read_env()
-
-    vk_session = vk.VkApi(token=env.str('VK_TOKEN'))
+def run_vk_bot(vk_bot_token):
+    vk_session = vk.VkApi(token=vk_bot_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             message_handler(event, vk_api)
+
+
+if __name__ == '__main__':
+    env = Env()
+    env.read_env()
+    run_vk_bot(env.str('VK_BOT_TOKEN'))
